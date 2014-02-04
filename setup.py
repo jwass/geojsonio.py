3cd61f36575cbec5b1e7f00745db7a8d30787e0e
Antonio Lima <anto87@gmail.com>
2014-02-04 15:39:49 +0100

    Packaged

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'geojsonio',
+]
+
+setup(
+    name='geojsonio',
+    version='0.0.1',
+    description='geojsonio CLI - Python.',
+    # long_description=readme + '\n\n' + history,
+    author='Antonio Lima',
+    author_email='anto87@gmail.com',
+    packages=packages,
+    package_data={'': ['LICENSE',]},
+    package_dir={'tji': 'tji'},
+    include_package_data=True,
+    license='Apache 2.0',
+    zip_safe=False,
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+    ),
+    install_requires=[
+        "github3.py>=0.7.0",
+    ],
+    entry_points={
+      'console_scripts':
+        ['geojsonio = geojsonio:main']
+    }
+)
